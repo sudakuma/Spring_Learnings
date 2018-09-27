414d08a6eb877f02f37889a86ae9f7d4d42ade96
sudakuma <sudakuma.it@gamil.com>
2018-09-27 16:00:55 +0530

    addEmployee method added

--- SpringBootApp/src/main/java/com/sudarshan/myapp/SpringBootApp/controller/EmployeeController.java
+++ SpringBootApp/src/main/java/com/sudarshan/myapp/SpringBootApp/controller/EmployeeController.java
@@ -3,10 +3,16 @@ package com.sudarshan.myapp.SpringBootApp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.sudarshan.myapp.SpringBootApp.model.Employee;
 import com.sudarshan.myapp.SpringBootApp.service.EmployeeService;
@@ -18,7 +24,7 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService employeeService;
 	
-	@GetMapping(value="/employee")
+	@GetMapping(value="/employees")
 	public List<Employee> getEmployees() {
 		List<Employee> employeeList = employeeService.getAllEmployees();
 		return employeeList;
@@ -37,6 +43,17 @@ public class EmployeeController {
 		List<Employee> employeeList = employeeService.getEmployeesByAgeAndSalary(age, salary);
 		
 		return employeeList;
+	}
+	
+	@PostMapping(value="/employee")
+	public ResponseEntity<Void> addEmployee(@RequestBody Employee employee, UriComponentsBuilder builder) {
+        
+		employeeService.addEmployee(employee);
+                
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(builder.path("/employee/{id}").buildAndExpand(employee.getId()).toUri());
+        
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
 	}
 	
 }
