3f5208e2a942b3fa0503e40cd51afcd1b457311b
Sudarshan <kumar.sudarshan@dxc.com>
2018-09-28 16:06:37 +0530

    swagger commit

--- SpringBootApp/src/main/java/com/sudarshan/myapp/SpringBootApp/config/SpringFoxConfig.java
+++ SpringBootApp/src/main/java/com/sudarshan/myapp/SpringBootApp/config/SpringFoxConfig.java
@@ -3,8 +3,11 @@ package com.sudarshan.myapp.SpringBootApp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,10 +18,23 @@ public class SpringFoxConfig {
 	@Bean
 	public Docket apiDocket(){
 		return new Docket(DocumentationType.SWAGGER_2)
-				   .select()
-				   .apis(RequestHandlerSelectors.basePackage("com.sudarshan.myapp.SpringBootApp"))
-				   //.apis(RequestHandlerSelectors.any()) -- For all the endpoints
-				   .paths(PathSelectors.any())
-				   .build();
+			.apiInfo(getApiInfo())
+		    .select()
+			.apis(RequestHandlerSelectors.basePackage("com.sudarshan.myapp.SpringBootApp"))
+			//.apis(RequestHandlerSelectors.any()) -- For all the endpoints
+			.paths(PathSelectors.any())
+			.build();
 	}
+	
+	private ApiInfo getApiInfo() {
+        Contact contact = new Contact("Sudarshan Kumar", "https://google.com", "sudakuma.it@gmail.com");
+        return new ApiInfoBuilder()
+        .title("Employee Service Manager")
+        .description("Employee Management")
+        .version("1.0.0")
+        .license("Apache 2.0")
+        .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+        .contact(contact)
+        .build();
+    }
 }
