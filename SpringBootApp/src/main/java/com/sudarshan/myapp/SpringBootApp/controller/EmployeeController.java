73d77d9f9c88fcb68457ffc8b6474d23d87f877e
Sudarshan <kumar.sudarshan@dxc.com>
2018-10-04 16:36:33 +0530

    Added Spring Security with JWT

--- SpringBootApp/src/main/java/com/sudarshan/myapp/SpringBootApp/controller/EmployeeController.java
+++ SpringBootApp/src/main/java/com/sudarshan/myapp/SpringBootApp/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +22,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping(value="/employeservice")
+@RequestMapping(value="/employeeservice")
 @Api(description = "Set of endpoints for Creating, Retrieving, Updating and Deleting of Employee.")
 public class EmployeeController {
 	
@@ -30,6 +31,7 @@ public class EmployeeController {
 	
 	@GetMapping(value="/employees")
 	@ApiOperation("Returns list of all employee in the system.")
+	@PreAuthorize("hasAuthority('ADMIN_USER') or hasAuthority('STANDARD_USER')")
 	public List<Employee> getEmployees() {
 		List<Employee> employeeList = employeeService.getAllEmployees();
 		return employeeList;
@@ -37,6 +39,7 @@ public class EmployeeController {
 	
 	@GetMapping(value="/employee/{id}")
 	@ApiOperation("Returns a specific employee by their identifier. 404 if does not exist.")
+	@PreAuthorize("hasAuthority('ADMIN_USER') or hasAuthority('STANDARD_USER')")
 	public Employee getEmployeeById(@PathVariable("id")long id) {
 		
 		Employee emp = employeeService.getEmployeeById(id);
@@ -46,6 +49,7 @@ public class EmployeeController {
 	
 	@GetMapping(value="/employee/{age}/{salary}")
 	@ApiOperation("Returns list of employees by their age and salary. 404 if does not exist.")
+	@PreAuthorize("hasAuthority('ADMIN_USER') or hasAuthority('STANDARD_USER')")
 	public List<Employee> getEmployeeByAgeAndSalary(@PathVariable("age") int age , @PathVariable("salary") int salary){
 		List<Employee> employeeList = employeeService.getEmployeesByAgeAndSalary(age, salary);
 		
@@ -54,6 +58,7 @@ public class EmployeeController {
 	
 	@PostMapping(value="/employee")
 	@ApiOperation("Creates a new Employee.")
+	@PreAuthorize("hasAuthority('ADMIN_USER')")
 	public ResponseEntity<Void> addEmployee(@RequestBody Employee employee, UriComponentsBuilder builder) {
         
 		employeeService.addEmployee(employee);
@@ -65,6 +70,7 @@ public class EmployeeController {
 	
 	@PostMapping(value="/delete/{employeeId}")
 	@ApiOperation("Delete Employee.")
+	@PreAuthorize("hasAuthority('ADMIN_USER')")
 	public ResponseEntity<Void> deleteEmployee(@PathVariable("employeeId") int employeeId,UriComponentsBuilder builder) {
         
 		employeeService.deleteEmployee(employeeId);
